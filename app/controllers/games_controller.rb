123b2a836ee9c5ae10d95a1df3e9418d6058bc36
Eugenie Avedikian <eugenieavedikian@gmail.com>
2020-08-22 17:37:00 +0200

    stylesheet and trying finish the exercice but pb with url???

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,32 +2,34 @@ require 'json'
 require 'open-uri'
 
 class GamesController < ApplicationController
+  VOYELS = %w(A E I O U)
   def new
-    @letters = ('A'..'Z').to_a.sample(8)
+     @voys = VOYELS.sample(5)
+     @cons = (('A'..'Z').to_a - VOYELS).sample(5)
+     @letters = (@voys + @cons).shuffle
   end
 
   def score
-    @grid = params[:grid].split # lettre generées aléatoirement
+    @grid = params[:grid].split
     @guess = params[:guess].upcase.split
+    @included = in_the_grid?(@grid, @guess)
+    @english_word = is_english_word?(@guess)
 
-    if in_the_grid?(@grid, @guess) && is_english_word(@guess)
-      @message = "votre mot est correct"
-    elsif !is_english_word?(@guess)
-      @message = "it 's not an english word"
-    else
-      @message = 'try again'
-    end
+    # if in_the_grid?(@grid, @guess) && is_english_word(@guess)
+    #   @message = "votre mot est correct"
+    # elsif !is_english_word?(@guess)
+    #   @message = "it 's not an english word"
+    # else
+    #   @message = 'try again'
+    # end
   end
 
-
-    # grid = params[:grid] # lettre generées aléatoirement
-    # input_word = input_word.split # => créer un array
-    # grid = grid.split
   private
 
   def in_the_grid?(grid,guess)
     grid_hash  = Hash.new(0)
     grid.each { |letter| grid_hash[letter] += 1 }
+
     guess_hash = Hash.new(0)
     guess.each { |letter| guess_hash[letter] += 1}
 
@@ -39,11 +41,9 @@ class GamesController < ApplicationController
   end
 
   def is_english_word?(word)
-    url = "https://wagon-dictionary.herokuapp.com/#{word}"
-    user_serialized = open(url).read
-    user = JSON.parse(user_serialized)
-
-    user['found']
+    response = open("https://wagon-dictionary.herokuapp.com/:#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 
 end
